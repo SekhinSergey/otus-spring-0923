b9e4e0fba54e5bac9a641e5bae0a3b55372ebc6f
Sergey.Sekhin <Sergey.Sekhin@russianpost.ru>
2023-10-01 21:13:00 +0300

    hw1: bean indexes fix

--- hw1/src/main/java/ru/otus/spring/util/CsvResourceReader.java
+++ hw1/src/main/java/ru/otus/spring/util/CsvResourceReader.java
@@ -4,7 +4,6 @@ import org.springframework.core.io.ClassPathResource;
 import ru.otus.spring.exception.CsvReadException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,14 +13,13 @@ public class CsvResourceReader implements ResourceReader {
 
     public List<List<String>> readResource(String fileName) throws CsvReadException {
         List<List<String>> csvList = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(
-                new  ClassPathResource(fileName).getInputStream()))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new ClassPathResource(fileName).getInputStream()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
                 csvList.add(Arrays.asList(values));
             }
-        } catch (IOException exception) {
+        } catch (Exception exception) {
             throw new CsvReadException("CSV parsing stopped with an error: " + exception.getMessage());
         }
         return csvList;
